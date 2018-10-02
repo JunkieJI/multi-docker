6ca3e2dc3c7e1cc2b93f5bd5690967aed817554c
Jihaad Pienaar <jihaad.pienaar@spandigital.com>
2018-10-02 16:39:27 +0200

    Fixed issue with server /current sending undefined values

--- server/index.js
+++ server/index.js
@@ -48,9 +48,8 @@ app.get('/values/all', async (req, res) => {
 
 app.get('/values/current', async (req, res) => {
     redisClient.hgetall('values', (err, values) => {
-        res.send(values);
+      res.send(values);
     });
-    res.send(values.rows);
 });
 
 
